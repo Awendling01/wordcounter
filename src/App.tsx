b1c0f832b05542fe9fc4abaa387f53040599a235
Gohikeco1 <gohikeco1@gmail.com>
2021-03-04 00:17:10 -0600

    wordcounter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [userInput, setUserInput] = useState("");
+	const [numberCount, setNumberCount] = useState(Number);
+
+	const onUserInputCount = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.currentTarget.value;
+		setUserInput(value);
+		setNumberCount(value.trim().split(/\s+/).length);
+	};
+
+	return (
+		//apparently 'form' has to be enclosed in a div? why is that? it shows up red underline if not
+		<div>
+			<form>
+				<input value={userInput} onChange={onUserInputCount} />
+			</form>
+			<div>{numberCount}</div>
+		</div>
+	);
+};
 
 export default App;
